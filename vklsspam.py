6f1f0a7b032afa48bfde0c7dee38ff5bd278791f
Lonely0Programmer <70218605+Lonely0Programmer@users.noreply.github.com>
2020-09-12 21:01:54 +0300

    Add files via upload

---
+++ vklsspam.py
@@ -0,0 +1,16 @@
+﻿import vk_api
+import time
+import random
+
+my_token = 'token VkKateMobile'
+msg = input('Msg: ')
+
+session = vk_api.VkApi(token = my_token)
+vk = session.get_api()
+
+while True:
+	try:
+		vk.messages.send(user_id = айди кому спамить без '', message = msg, random_id = random.randint(0, 9999999999))
+	except:
+		pass
+	time.sleep(3)
